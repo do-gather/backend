dcb50d57e389a7a51175ebd82f8070fc2abb5c66
hyelin <k1h2fls@gmail.com>
2022-05-14 18:51:32 +0900

    fix: jwt Tokenprovider

--- src/main/java/com/delivious/backend/domain/users/jwt/TokenProvider.java
+++ src/main/java/com/delivious/backend/domain/users/jwt/TokenProvider.java
@@ -65,10 +65,10 @@ public class TokenProvider implements InitializingBean {
 
     public Authentication getAuthentication(String token) {
         Claims claims = Jwts
-                //.parserBuilder()
-                .parser()
+                .parserBuilder()
+                //.parser()
                 .setSigningKey(key)
-                //.build()
+                .build()
                 .parseClaimsJws(token)
                 .getBody();
                 //.build()
@@ -87,7 +87,7 @@ public class TokenProvider implements InitializingBean {
 
     public boolean validateToken(String token) {
         try {
-            Jwts.parser().setSigningKey(key).parseClaimsJws(token);
+            Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
             return true;
         } catch (io.jsonwebtoken.security.SecurityException | MalformedJwtException e) {
             logger.info("잘못된 JWT 서명입니다.");
